42a4650b6515c503c0d793818543fdef43ec7251
l00186108 <l00186108@atu.ie>
2023-12-25 18:10:22 +0000

    Adding Exercise 4

---
+++ 4_Verify_Powershell.ps1
@@ -0,0 +1,9 @@
+﻿# Set the execution policy to RemoteSigned with Force flag to override restrictions
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force
+
+# Initialize a counter variable
+$I = 0
+
+# Display the paths in the PSModulePath environment variable with an index
+$env:PSModulePath -split ';' |
+    ForEach-Object {"[{0:N0}] {1}" -f $I++, $_}
