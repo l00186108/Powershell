5bf321a297313eaf5bfab1b81cedc6d5d4d57ebe
l00186108 <l00186108@atu.ie>
2023-12-27 19:06:35 +0000

    Adding Types

---
+++ Types/types.ps1
@@ -0,0 +1,21 @@
+
+# String type
+$StringValue = "Yoo hoo!"
+$StringValue.ToUpper()
+$StringValue.ToLower()
+
+# Array type
+$MyArray = 1,2,3,4,5
+$MyArray[1]
+
+# Integer types
+$LittleNumber = 12345
+$LittleNumber.GetType()
+$BigNumber = 123456789123456789
+$BigNumber.GetType()
+
+# Float types
+[float]$Floaty32 = 12.12
+$Floaty32.GetType()
+[double]$Floaty64 = 12345.1234
+$Floaty64.GetType()
